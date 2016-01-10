b23655a87579a0d825d6a53f0ebc2379938502b7
Jusonex <jusonex96@gmail.com>
2016-01-10 22:31:15 +0100

    Sails.io deaktiviert und JS Minimierung deaktiviert

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -4,10 +4,10 @@ module.exports = function (grunt) {
 		'concat',
 		'uglify',
 		'cssmin',
-		'sails-linker:prodJs',
+		//'sails-linker:prodJs',
 		'sails-linker:prodStyles',
 		'sails-linker:devTpl',
-		'sails-linker:prodJsJade',
+		//'sails-linker:prodJsJade',
 		'sails-linker:prodStylesJade',
 		'sails-linker:devTplJade'
 	]);
